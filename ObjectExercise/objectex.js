47f3cd2410cc39a66d46eef7b0c4ec4e93caa5e0
Ahmet Berke Tekin <ahmetberketekin@gmail.com>
2024-10-05 18:44:13 +0300

    Simple object exercise

---
+++ ObjectExercise/objectex.js
@@ -0,0 +1,13 @@
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+]
+
+const titles = books.map((object) => object.title)
+console.log(titles)
